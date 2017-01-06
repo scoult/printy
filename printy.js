495bd3b610dfa693fd15fd0983e911b911ef3057
Sam Coult <sam@coult.com>
2017-01-06 16:01:54 +1300

    Added files

---
+++ printy.js
@@ -0,0 +1,25 @@
+(function () { 
+	
+	if (!($ = window.jQuery)) {
+	    script = document.createElement( 'script' );
+	   script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
+	    script.onload=prepareLabel;
+	    document.body.appendChild(script);
+	} 
+	else {
+	    prepareLabel();
+	}
+ 
+	function prepareLabel() {
+		
+		var nbsp = String.fromCharCode(160);
+		var productinfo = $('div#subheading h1').text().split(nbsp + nbsp);
+		var productName = productInfo[0];
+		var productPrice = productInfo[1];
+		var productDescription = $('div.description').text();
+		var productSKU = $("label:contains('SKU')").parent().find("span.unit").text();
+		
+		alert('Print label for: Product - ' + productName + ', price - ' + productPrice + ', Desc - ' + productDescription + ', SKU - ' + productSKU + '?');
+		  
+	}
+ }());
